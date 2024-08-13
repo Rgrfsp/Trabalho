774463a5f23d031e5435e5cf57987880da0e8a6b
RgrFSPrudenci <159174080+Rgrfsp@users.noreply.github.com>
2024-08-13 13:40:24 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE professores(
+ id int(3) UNIQUE,
+ nomes  VARCHAR(150)NOT NULL,
+ idade VARCHAR(3),
+ turma VARCHAR(4)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
